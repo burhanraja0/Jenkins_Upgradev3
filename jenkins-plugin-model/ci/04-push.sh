f2eb5866fa8dee433955f0454c00eb7ead9dd5cf
burhanraja0 <150225951+burhanraja0@users.noreply.github.com>
2024-01-11 13:13:55 +0530

    Update 04-push.sh

--- jenkins-plugin-model/ci/04-push.sh
+++ jenkins-plugin-model/ci/04-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker image build -t burhanraja28/jenkins-demo_2:latest -f jenkins-plugin-model/src/demo2-publish/dockerfile .
+docker image build -t burhanraja28/jenkins-demo_2:$1 -f jenkins-plugin-model/src/demo2-publish/dockerfile .
 
 if [ -z ${DOCKER_HUB_USER+x} ]
 then 
@@ -8,4 +8,4 @@ else
     docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
 fi
 
-docker image push burhanraja28/jenkins-demo_2:latest
+docker image push burhanraja28/jenkins-demo_2:$1
